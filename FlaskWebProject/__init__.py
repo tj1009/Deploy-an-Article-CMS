37fc25130b7f2f2c158ee2822479272d86b5fbbb
Tanya Jain <tjain.cse15@chitkara.edu.in>
2020-06-29 15:45:14 -0400

    chnges to init

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,6 +11,12 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
+handler = logging.StreamHandler(stream=sys.stdout)
+handler.setFormatter(logging.Formatter("[%(asctime)s] %(levelname)s in %(module)s: %(message)s"))
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.INFO)
+app.logger.info('Tanya Udacity Article CMS')
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
