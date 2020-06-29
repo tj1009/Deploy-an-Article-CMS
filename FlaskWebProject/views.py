573d20cdea77404cb9e717e92389e6c3f9303495
Tanya Jain <tjain.cse15@chitkara.edu.in>
2020-06-29 18:20:55 -0400

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -70,6 +70,7 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        app.logger.info('%s successfully logged in', user.username)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
