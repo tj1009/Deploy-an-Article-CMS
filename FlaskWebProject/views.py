99c527c986ef8d7084f96771da929d5a0b760a9d
Tanya Jain <tjain.cse15@chitkara.edu.in>
2020-06-29 17:56:32 -0400

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -66,7 +66,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):     
-          app.logger.info('Sorry! Password mismatch')
+          app.logger.info('Invalid login attempt')
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
